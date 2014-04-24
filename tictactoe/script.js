a4df615deae16ba979035154213f96f767f20f04
Jeffrey Takaki <jeffreytakaki@Jeffreys-MacBook-Pro.local>
2014-04-23 21:51:10 -0700

    working counter and reset

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -3,78 +3,101 @@ function tttController($scope) {
 
 	var clickedspaces = 0;
 
+	$scope.counterX=0;
+	$scope.counterO=0;
+	
+
 	var checkWin = function() {
-	var win = false;
+		console.log($scope.counterX);
+		console.log($scope.counterO);
+		
+		var win = false;
+		
 		if ($scope.board[0] == $scope.board[1] && $scope.board[2]==$scope.board[1] && $scope.board[1]!==" "){
 			win = true;
 			if ($scope.board[0] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");	
 			} else {
-				alert("O wins!");
+				$scope.counterO++;
+				alert("O wins!");				
 			}
 			//reset button here?
 		}
 		if ($scope.board[3] == $scope.board[4] && $scope.board[5]==$scope.board[4] && $scope.board[5]!==" "){
 			win = true;
 			if ($scope.board[3] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");		
 			} else {
-				alert("O wins!");
+				$scope.counterO++;
+				alert("O wins!");				
 			}
 
 		}
 		if ($scope.board[6] == $scope.board[7] && $scope.board[8]==$scope.board[7] && $scope.board[8]!==" "){
 			win = true;
 			if ($scope.board[6] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");				
 			} else {
-				alert("O wins!");
+				$scope.counterO++;
+				alert("O wins!");				
 			}
 		}
 		if ($scope.board[0] == $scope.board[3] && $scope.board[6]==$scope.board[3] && $scope.board[6]!==" "){
 			win = true;
 			if ($scope.board[0] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");				
 			} else {
+				$scope.counterO++;
 				alert("O wins!");
 			}
 		}
 		if ($scope.board[1] == $scope.board[4] && $scope.board[7]==$scope.board[4] && $scope.board[7]!==" "){
 			win = true;
 			if ($scope.board[1] == 'X') {
+				$scope.counterX++;
 				alert("X wins!");
 			} else {
+				$scope.counterO++;
 				alert("O wins!");
 			}
 		}
 		if ($scope.board[2] == $scope.board[5] && $scope.board[8]==$scope.board[5] && $scope.board[8]!==" "){
 			win = true;
 			if ($scope.board[2] == 'X') {
+				$scope.counterX++;
 				alert("X wins!");
 			} else {
+				$scope.counterO++;
 				alert("O wins!");
 			}
 		}
 		if ($scope.board[0] == $scope.board[4] && $scope.board[8]==$scope.board[4] && $scope.board[8]!==" "){
 			win = true;
 			if ($scope.board[0] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");				
 			} else {
+				$scope.counterO++;
 				alert("O wins!");
 			}		
 		}
 		if ($scope.board[2] == $scope.board[4] && $scope.board[6]==$scope.board[4] && $scope.board[6]!==" "){
 			win = true;
 			if ($scope.board[2] == 'X') {
-				alert("X wins!");
+				$scope.counterX++;
+				alert("X wins!");				
 			} else {
-				alert("O wins!");
+				$scope.counterO++;
+				alert("O wins!");				
 			}
 		}	else if (clickedspaces === 9 && !win) {
 			alert("Draw!");}
 }
 	
-
 
 	$scope.clicker = function(cellindex) {
 		
@@ -97,11 +120,30 @@ function tttController($scope) {
 			alert("choose another box!");
 			}
 	}	
-};
 
 
-			function reset() {
-				location.reload()};
+
+	// var display = function() {
+	// 	if ($scope.justWon == "X") {
+	// 		return "X wins!"
+		//}	
+
+	//} 	//endbracket for display function. purpose of the function is to display text 
+		//in the displayArea div for X and O wins, as well as 'choose another box' and 'play again?''
+
+	$scope.reset = function(){
+		// $scope.board = $scope.initial;		
+		$scope.board = [" "," "," "," "," "," "," "," "," "];
+		clickedspaces = 0;
+		};	
+
+};  //end controller bracket
+
+
+	
+		
+
+	
 
 
 
